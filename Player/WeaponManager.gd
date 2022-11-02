8b80a83abb147105058746839c823be2b9124f03
Robotnik <85544501+robotnik-dev@users.noreply.github.com>
2022-11-02 15:29:46 +0100

    weapon manager done

--- Player/WeaponManager.gd
+++ Player/WeaponManager.gd
@@ -1,19 +1,44 @@
+tool
+
 extends Node2D
 
-export(int) var maximum_slots: int = 6
 export(PackedScene) var weapon_slot_scene
 
-func _ready() -> void:
-	_add_weapon_slots()
+export(int) var maximum_slots = 6
+export(int) var weapon_slots = 0 setget set_weapon_slots
+export(float) var spawn_distance = 100.0
 
-func _add_weapon_slots() -> void:
-	var spawn_path = $Path2D/PathFollow2D
-	for i in maximum_slots:
-		spawn_path.unit_offset = randf()
-		var spawn_position = spawn_path.position
-		var weapon_slot = weapon_slot_scene.instance()
-		weapon_slot.global_position = spawn_position
-		add_child(weapon_slot)
+
+func set_weapon_slots(value: int) -> void:
+	if value > maximum_slots:
+		return
+	
+	var children = get_children()
+	if children:
+		for child in children:
+			remove_child(child)
+	
+	weapon_slots = value
+	for i in range(value):
+		_add_weapon_slot(weapon_slot_scene)
+
+func _add_weapon_slot(weapon_slot_scene: PackedScene) -> void:
+	var degree_step = 360 / (weapon_slots)
+	var slots = get_children()
+	var degree = 0
+	for slot in slots:
+		var new_theta = deg2rad(degree)
+		var new_pos: Vector2 = polar2cartesian(spawn_distance, new_theta)
+		slot.position = new_pos
+		degree += degree_step
+	
+	var weapon_slot = weapon_slot_scene.instance()
+	add_child(weapon_slot)
+	weapon_slot.owner = self
+	
+	var theta = deg2rad(degree)
+	var spawn_pos: Vector2 = polar2cartesian(spawn_distance, theta)
+	weapon_slot.position = spawn_pos
 
 
 func add_weapon(weapon_scene: PackedScene) -> void:
